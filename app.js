6b6de87c2c42025ab80d84cdfd1e021a46c1b80a
perfidiousscript <sam.moss321@gmail.com>
2015-10-21 20:53:11 -0500

    Stable locator

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){
-	
+	var $box = $('.col')
+	$("body").on('click','.location', function(){
+		$('#top').children('span').text(($box.offset().top).toFixed(2));
+		$('#left').children('span').text(($box.offset().left).toFixed(2));
+		console.log("this works")
+	});
+	//$("body").on('click',function(){
+
+	//})
 })
+
+function randomizer(min,max){
+	return Math.random() * (max - min) + min;
+}
